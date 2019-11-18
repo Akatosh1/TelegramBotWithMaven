57e97b50fa8c240bc089bd5d17af8ebc28b5fc43
Akatosh1 <44877271+Akatosh1@users.noreply.github.com>
2019-11-18 22:15:50 +0500

    Create AddBalanceCommandTest.java

---
+++ src/test/java/commands/BalanceCommands/AddBalanceCommandTest.java
@@ -0,0 +1,16 @@
+package commands.BalanceCommands;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddBalanceCommandTest {
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
