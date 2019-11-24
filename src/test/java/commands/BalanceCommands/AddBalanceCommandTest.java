f4d2226b76c6228e8e07cf3e8dafe68b888438ee
Akatosh1 <44877271+Akatosh1@users.noreply.github.com>
2019-11-24 16:57:39 +0500

    добавил пробный тест

--- src/test/java/commands/BalanceCommands/AddBalanceCommandTest.java
+++ src/test/java/commands/BalanceCommands/AddBalanceCommandTest.java
@@ -1,6 +1,9 @@
 package commands.BalanceCommands;
 
+import org.junit.Assert;
 import org.junit.Test;
+import commands.Main;
+import org.telegram.telegrambots.meta.api.objects.Message;
 
 import static org.junit.Assert.*;
 
@@ -8,9 +11,28 @@ public class AddBalanceCommandTest {
 
     @Test
     public void getDescription() {
+        AddBalanceCommand addBalanceCommand = new AddBalanceCommand();
+        String actual = addBalanceCommand.getDescription();
+        String expected = "добавить денег на баланс";
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void execute() {
+        AddBalanceCommand addBalanceCommand = new AddBalanceCommand();
+        Main main = new Main();
+        Message message = new Message(){
+            @Override
+            public String getText() {
+                return "/profit";
+            }
+            @Override
+            public Long getChatId() {
+                return 0L;
+            }
+        };
+        Assert.assertEquals(message.getText(), "/profit");
+        addBalanceCommand.execute(message, main);
+        Assert.assertEquals(main.condition, "addBalance");
     }
 }
