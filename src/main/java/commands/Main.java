a6a05db3d077420c49dc5b740ef22d7032e92406
polu-lina <polupanovalina@gmail.com>
2019-11-25 11:04:02 +0500

    Add button

--- src/main/java/commands/Main.java
+++ src/main/java/commands/Main.java
@@ -14,10 +14,13 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -59,6 +62,44 @@ public class Main extends TelegramLongPollingBot {
         if (message != null && message.hasText()) {
             onMessageReceived(message);
         }
+        if(update.hasMessage()){
+//            if(update.getMessage().hasText()){
+//                try {
+//                    execute(sendInlineKeyBoardMessage(update.getMessage().getChatId()));
+//                } catch (TelegramApiException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+        }else if(update.hasCallbackQuery()){
+            try {
+                execute(new SendMessage().setText(
+                        update.getCallbackQuery().getData())
+                        .setChatId(update.getCallbackQuery().getMessage().getChatId()));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static SendMessage sendInlineKeyBoardMessage(long chatId) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+        inlineKeyboardButton1.setText("Тык");
+        inlineKeyboardButton1.setCallbackData("Button \"Тык\" has been pressed");
+        inlineKeyboardButton2.setText("Тык2");
+        inlineKeyboardButton2.setCallbackData("Button \"Тык2\" has been pressed");
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Fi4a").setCallbackData("CallFi4a"));
+        keyboardButtonsRow2.add(inlineKeyboardButton2);
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        return new SendMessage().setChatId(chatId).setText("Пример").setReplyMarkup(inlineKeyboardMarkup);
     }
 
     public void onMessageReceived(Message message){
@@ -66,6 +107,11 @@ public class Main extends TelegramLongPollingBot {
         if(condition.equals("")) {
             if (commandDict.containsKey(messageText)) {
                 commandDict.get(messageText).execute(message, this);
+                try {
+                    execute(sendInlineKeyBoardMessage(message.getChatId()));
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
                 return;
             }
         }
@@ -98,6 +144,8 @@ public class Main extends TelegramLongPollingBot {
             condition = "";
         }
     }
+
+
 
     public void sendMsg(Message message, String text) {
         SendMessage sendMessage = new SendMessage();
